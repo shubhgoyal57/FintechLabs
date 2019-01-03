780f9410b416d72fe9b6be392d96c2c55aa9579f
shubhgoyal57 <shubhgoyal57@gmail.com>
2019-01-03 16:16:23 +0530

    third

--- FirstGrails/grails-app/controllers/firstgrails/StudentsController.groovy
+++ FirstGrails/grails-app/controllers/firstgrails/StudentsController.groovy
@@ -8,7 +8,7 @@ class StudentsController {
         Student s1=new Student()
         s1.name="shuhbam"
         s1.age=23
-        s1.course="shuhbam"
+        s1.course="cse"
         s1.save(flush:true)
         render "${s1.name}"
     }
